6585dbc52562c62acb9039447b2467eab3cfa4d5
Tom Snelling <tomsnelling8@gmail.com>
2021-03-04 21:27:10 +0000

    upgrade zeromq, include more data in webhook

--- api/listen.js
+++ api/listen.js
@@ -1,6 +1,5 @@
 const zlib = require('zlib')
 const zmq = require('zeromq')
-const sock = zmq.socket('sub')
 const request = require('request-promise')
 const Alert = require('./schema/Alert')
 const stations = require('./stations.json')
@@ -19,6 +18,9 @@ const sendAlert = async ({
   supply,
   demand,
   maxPadSize,
+  stationType,
+  distance,
+  planetary,
   webhookUrl,
   freq,
 }) => {
@@ -33,9 +35,9 @@ const sendAlert = async ({
     .concat(rareCommodityData)
     .filter((com) => com.symbol.toLowerCase() === commodityName.toLowerCase())
 
-  const title = `Alert triggered: ${
-    commodity ? commodity.name : commodityName
-  } ${type} ${trigger === 'above' ? '>' : '<'} ${alertValue}`
+  const title = `ALERT: ${commodity ? commodity.name : commodityName} ${type} ${
+    trigger === 'above' ? '>' : '<'
+  } ${alertValue}`
 
   try {
     await request({
@@ -55,14 +57,31 @@ const sendAlert = async ({
             },
             fields: [
               {
-                name: 'Value',
-                value: value,
+                name: 'Location',
+                value: `${station}, ${system} ${
+                  planetary ? '(planetary)' : ''
+                }`,
                 inline: false,
               },
               {
-                name: 'Location',
-                value: `${station}, ${system}`,
-                inline: false,
+                name: 'Value',
+                value: value.toLocaleString(),
+                inline: true,
+              },
+              {
+                name: 'Supply',
+                value: supply.toLocaleString(),
+                inline: true,
+              },
+              {
+                name: 'Demand',
+                value: demand.toLocaleString(),
+                inline: true,
+              },
+              {
+                name: 'Station type',
+                value: stationType,
+                inline: true,
               },
               {
                 name: 'Max. pad size',
@@ -70,13 +89,8 @@ const sendAlert = async ({
                 inline: true,
               },
               {
-                name: 'Supply',
-                value: supply,
-                inline: true,
-              },
-              {
-                name: 'Demand',
-                value: demand,
+                name: 'Distance from star',
+                value: distance,
                 inline: true,
               },
               {
@@ -93,127 +107,149 @@ const sendAlert = async ({
       },
     })
   } catch (e) {
-    console.error('webhook request failed')
+    console.error(`webhook failed for alert ${alertId}`)
   }
 }
 
-sock.connect('tcp://eddn.edcd.io:9500')
-console.log('listener: worker connected to eddn.edcd.io:9500')
+//
+;(async () => {
+  const sock = new zmq.Subscriber()
+  sock.connect('tcp://eddn.edcd.io:9500')
+  console.log('listener: worker connected to eddn.edcd.io:9500')
 
-sock.subscribe('')
+  sock.subscribe('')
 
-sock.on('message', (message) => {
-  const inflated = JSON.parse(zlib.inflateSync(message))
-  if (inflated['$schemaRef'] === 'https://eddn.edcd.io/schemas/commodity/3') {
-    const [station] = stations.filter(
-      (st) => st.name === inflated.message.stationName
-    )
-    const maxPadSize = station ? station.max_landing_pad_size : 'unknown'
-    const commodities = inflated.message.commodities
-    commodities.forEach(async (commodity) => {
-      const alerts = await Alert.find({ commodity: commodity.name })
-      alerts.forEach(async (alert) => {
-        if (
-          alert.freq === 0 ||
-          (alert.freq > 0 && Date.now() > alert.lastSent + alert.freq)
-        ) {
+  for await (const [topic] of sock) {
+    const inflated = JSON.parse(zlib.inflateSync(topic))
+    if (inflated['$schemaRef'] === 'https://eddn.edcd.io/schemas/commodity/3') {
+      const [station] = stations.filter(
+        (st) => st.name === inflated.message.stationName
+      )
+      const planetary = station ? station.is_planetary : false
+      const stationType = station ? station.type : 'unknown'
+      const maxPadSize = station ? station.max_landing_pad_size : 'unknown'
+      const distance = station
+        ? `${station.distance_to_star.toLocaleString()} Ls`
+        : 'unknown'
+
+      const commodities = inflated.message.commodities
+      commodities.forEach(async (commodity) => {
+        const alerts = await Alert.find({ commodity: commodity.name })
+        alerts.forEach((alert) => {
           if (
-            alert.pad === 'any' ||
-            (alert.pad === 'l' && maxPadSize === 'L') ||
-            maxPadSize === 'unknown'
+            alert.freq === 0 ||
+            (alert.freq > 0 && Date.now() > alert.lastSent + alert.freq)
           ) {
             if (
-              alert.type === 'buy' &&
-              commodity.buyPrice !== 0 &&
-              commodity.stock > alert.minSupply
+              alert.pad === 'any' ||
+              (alert.pad === 'l' && maxPadSize === 'L') ||
+              maxPadSize === 'unknown'
             ) {
               if (
-                alert.trigger === 'above' &&
-                commodity.buyPrice > alert.value
+                alert.type === 'buy' &&
+                commodity.buyPrice !== 0 &&
+                commodity.stock > alert.minSupply
               ) {
-                sendAlert({
-                  alertId: alert._id,
-                  alertValue: alert.value,
-                  commodityName: commodity.name,
-                  type: 'buy',
-                  trigger: 'above',
-                  value: commodity.buyPrice,
-                  station: inflated.message.stationName,
-                  system: inflated.message.systemName,
-                  demand: commodity.demand,
-                  supply: commodity.stock,
-                  maxPadSize: maxPadSize,
-                  webhookUrl: alert.webhook,
-                  freq: alert.freq,
-                })
+                if (
+                  alert.trigger === 'above' &&
+                  commodity.buyPrice > alert.value
+                ) {
+                  sendAlert({
+                    alertId: alert._id,
+                    alertValue: alert.value,
+                    commodityName: commodity.name,
+                    type: 'buy',
+                    trigger: 'above',
+                    value: commodity.buyPrice,
+                    station: inflated.message.stationName,
+                    system: inflated.message.systemName,
+                    demand: commodity.demand,
+                    supply: commodity.stock,
+                    maxPadSize,
+                    stationType,
+                    distance,
+                    planetary,
+                    webhookUrl: alert.webhook,
+                    freq: alert.freq,
+                  })
+                } else if (
+                  alert.trigger === 'below' &&
+                  commodity.buyPrice < alert.value
+                ) {
+                  sendAlert({
+                    alertId: alert._id,
+                    alertValue: alert.value,
+                    commodityName: commodity.name,
+                    type: 'buy',
+                    trigger: 'below',
+                    value: commodity.buyPrice,
+                    station: inflated.message.stationName,
+                    system: inflated.message.systemName,
+                    demand: commodity.demand,
+                    supply: commodity.stock,
+                    maxPadSize,
+                    stationType,
+                    distance,
+                    planetary,
+                    webhookUrl: alert.webhook,
+                    freq: alert.freq,
+                  })
+                }
               } else if (
-                alert.trigger === 'below' &&
-                commodity.buyPrice < alert.value
+                alert.type === 'sell' &&
+                commodity.sellPrice !== 0 &&
+                commodity.demand > alert.minDemand
               ) {
-                sendAlert({
-                  alertId: alert._id,
-                  alertValue: alert.value,
-                  commodityName: commodity.name,
-                  type: 'buy',
-                  trigger: 'below',
-                  value: commodity.buyPrice,
-                  station: inflated.message.stationName,
-                  system: inflated.message.systemName,
-                  demand: commodity.demand,
-                  supply: commodity.stock,
-                  maxPadSize: maxPadSize,
-                  webhookUrl: alert.webhook,
-                  freq: alert.freq,
-                })
-              }
-            } else if (
-              alert.type === 'sell' &&
-              commodity.sellPrice !== 0 &&
-              commodity.demand > alert.minDemand
-            ) {
-              if (
-                alert.trigger === 'above' &&
-                commodity.sellPrice > alert.value
-              ) {
-                sendAlert({
-                  alertId: alert._id,
-                  alertValue: alert.value,
-                  commodityName: commodity.name,
-                  type: 'sell',
-                  trigger: 'above',
-                  value: commodity.sellPrice,
-                  station: inflated.message.stationName,
-                  system: inflated.message.systemName,
-                  demand: commodity.demand,
-                  supply: commodity.stock,
-                  maxPadSize: maxPadSize,
-                  webhookUrl: alert.webhook,
-                  freq: alert.freq,
-                })
-              } else if (
-                alert.trigger === 'below' &&
-                commodity.sellPrice < alert.value
-              ) {
-                sendAlert({
-                  alertId: alert._id,
-                  alertValue: alert.value,
-                  commodityName: commodity.name,
-                  type: 'sell',
-                  trigger: 'below',
-                  value: commodity.sellPrice,
-                  station: inflated.message.stationName,
-                  system: inflated.message.systemName,
-                  demand: commodity.demand,
-                  supply: commodity.stock,
-                  maxPadSize: maxPadSize,
-                  webhookUrl: alert.webhook,
-                  freq: alert.freq,
-                })
+                if (
+                  alert.trigger === 'above' &&
+                  commodity.sellPrice > alert.value
+                ) {
+                  sendAlert({
+                    alertId: alert._id,
+                    alertValue: alert.value,
+                    commodityName: commodity.name,
+                    type: 'sell',
+                    trigger: 'above',
+                    value: commodity.sellPrice,
+                    station: inflated.message.stationName,
+                    system: inflated.message.systemName,
+                    demand: commodity.demand,
+                    supply: commodity.stock,
+                    maxPadSize,
+                    stationType,
+                    distance,
+                    planetary,
+                    webhookUrl: alert.webhook,
+                    freq: alert.freq,
+                  })
+                } else if (
+                  alert.trigger === 'below' &&
+                  commodity.sellPrice < alert.value
+                ) {
+                  sendAlert({
+                    alertId: alert._id,
+                    alertValue: alert.value,
+                    commodityName: commodity.name,
+                    type: 'sell',
+                    trigger: 'below',
+                    value: commodity.sellPrice,
+                    station: inflated.message.stationName,
+                    system: inflated.message.systemName,
+                    demand: commodity.demand,
+                    supply: commodity.stock,
+                    maxPadSize,
+                    stationType,
+                    distance,
+                    planetary,
+                    webhookUrl: alert.webhook,
+                    freq: alert.freq,
+                  })
+                }
               }
             }
           }
-        }
+        })
       })
-    })
+    }
   }
-})
+})()
